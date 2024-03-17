374a7e6c6cb4f91ccd162663080c30ef8ee37c31
swarnps3 <140943114+swarnps3@users.noreply.github.com>
2024-03-17 19:42:04 +0530

    Add files via upload

---
+++ LogOut/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css'; // Import your CSS file
+
+const LogoutPage = () => {
+  return (
+    <div>
+      <img src="cg.jpg" alt="Career Grow Logo" className="cg" />
+      <h1 className="title">Career Grow</h1>
+      <br />
+      <br />
+      <br />
+      <img src="download.png" alt="Unable to load" className="img" />
+      <hr />
+      <h2 className="small">You have been logged Out</h2>
+    </div>
+  );
+};
+
+export default LogoutPage;
