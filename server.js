7825714843142d5d1d0fd31936e0be11b791f6db
Thaman_bharti <117168416+thamanbharti@users.noreply.github.com>
2024-09-16 23:19:19 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const connectDB = require('./config/db')
 
 //rest object
 const app = express();
 
-//routes 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "welocome to job-search app",
-    })
-})
 
-//port 
+//dot config
+dotenv.config();
+//mongodb connection
+connectDB();
+
+app.use(express.json());
+app.use(cors({methods: ['GET', 'PUT', 'POST']}));
+app.use(morgan('dev'));
+
+//routes 
+app.use("/api/v1/test", require("./routes/testRoutes"));
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/auth", require("./routes/FavouriteRoute"));
+app.use("/api/v1/auth",require("./routes/skilltestRoute"));
+app.use("/api/v1", require("./routes/resumeUploadRoutes"));
+app.use('/api/v1/application',require("./routes/applicationRoutes"))
+app.use('/api/v1/skilltest',require('./routes/skilltestRoute'))
+app.use('/api/v1',require('./routes/recommendRoutes'));
+
+
 const PORT = 8080
 //listen
 app.listen(PORT, () => {
